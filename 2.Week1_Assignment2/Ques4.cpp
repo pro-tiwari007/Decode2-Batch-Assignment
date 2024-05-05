7cea69b018bb598395d5e5d8efeff2e3815f49ac
Priyanshu Tiwari <144623628+pro-tiwari007@users.noreply.github.com>
2024-05-05 15:55:50 +0530

    Add files via upload

---
+++ 2.Week1_Assignment2/Ques4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int radius, height;
+    cout<<"Enter the radius: ";
+    cin>>radius;
+    cout<<"Enter the height: ";
+    cin>>height;
+    cout<<"Volume of cylinder is: "<<3.14159265359*radius*radius*height;
+return 0;
+}
---
+++ 2.Week1_Assignment2/Ques5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char char1,char2;
+    cout<<"Enter the character 1: ";
+    cin>>char1;
+    cout<<"Enter the character 2: ";
+    cin>>char2;
+    cout<<"Difference between ASCII: "<<(int)(char1-char2);
+return 0;
+}

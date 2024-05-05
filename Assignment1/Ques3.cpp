12fb6a071b4db96b392ecffb3c86207bba280642
Priyanshu Tiwari <144623628+pro-tiwari007@users.noreply.github.com>
2024-05-05 13:56:26 +0530

    Create Ques3.cpp

---
+++ Assignment1/Ques3.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"The size of int is: "<<sizeof(int)<<endl;
+  cout<<"The size of bool is: "<<sizeof(bool)<<endl;
+  cout<<"The size of float is: "<<sizeof(float)<<endl;
+return 0;
+}
